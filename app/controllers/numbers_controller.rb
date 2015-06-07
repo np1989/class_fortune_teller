c6421e0293cbcaf90c302b61ad54369e60e2f5c5
np1989 <np1989@gmail.com>
2015-06-06 20:48:21 -0500

    Update

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,4 +1,10 @@
 class NumbersController < ApplicationController
+  def random
+
+    user_number = params["num"].to_i
+    @the_number = rand(user_number)
+  end
+
   def lucky_numbers
     @first_number = rand(100)
     @second_number = rand(100)
